6ca5674b2f15caa7d1f2563440ddfd81f92ee0d8
g-taraschi <gitaraschi@gmail.com>
2025-09-18 14:46:00 -0300

    chg (TPZWannApproxTools): add H1 approx spaces

--- src/TPZWannApproxTools.h
+++ src/TPZWannApproxTools.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "TPZWannGeometryTools.h"
+#include <Material/DarcyFlow/TPZDarcyFlow.h>
 #include <Material/DarcyFlow/TPZMixedDarcyFlow.h>
 #include <Material/TPZLagrangeMultiplier.h>
 #include <Material/TPZLagrangeMultiplierCS.h>
@@ -14,9 +15,12 @@ class TPZWannApproxTools {
 
 public:
   static TPZMultiphysicsCompMesh *CreateMultiphysicsCompMesh(TPZGeoMesh *gmesh, ProblemData *SimData);
+  static TPZCompMesh *CreateH1CompMesh(TPZGeoMesh *gmesh, ProblemData *SimData);
 
 private:
   static void AddPressureSkinElements(TPZCompMesh *cmesh, ProblemData *SimData, const int laglevel);
+  static void EqualizePressureSkinConnects(TPZCompMesh *cmesh, ProblemData *SimData);
+  static void EqualizeH1Connects(TPZCompMesh *cmesh, ProblemData *SimData);
   static void AddWellboreElements(TPZVec<TPZCompMesh *> &meshvec, ProblemData *SimData, const int laglevel);
   static void EqualizePressureConnects(TPZCompMesh *cmesh, ProblemData *SimData);
   static void AddHDivBoundInterfaceElements(TPZCompMesh *cmesh, ProblemData *SimData);
